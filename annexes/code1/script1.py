84c0ca51d8ff4bf0c41cc5dad04876cced7c5acc
FARID HAMDAD <hamdadfarid54@gmail.com>
2025-08-07 17:20:29 +0200

    Update script1.py

--- annexes/code1/script1.py
+++ annexes/code1/script1.py
@@ -1,2 +1,23 @@
+# concept.py
+class Concept:
+    def __init__(self, name, valence=0.0):
+        self.name = name
+        self.activation = 0.0
+        self.valence = valence
+        self.links = {}
 
-rrftt
+    def link_to(self, other, weight=1.0):
+        self.links[other] = weight
+
+    def propagate(self):
+        for concept, weight in self.links.items():
+            concept.activation += self.activation * weight
+
+# Exemple d'utilisation
+if __name__ == "__main__":
+    c1 = Concept("Joie", valence=0.8)
+    c2 = Concept("Rire", valence=0.7)
+    c1.activation = 1.0
+    c1.link_to(c2, weight=0.5)
+    c1.propagate()
+    print(f"Activation de {c2.name} : {c2.activation}")

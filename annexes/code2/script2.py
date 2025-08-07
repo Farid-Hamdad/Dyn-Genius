17233a476310b75ee0a4dec6454986cdb68cc164
FARID HAMDAD <hamdadfarid54@gmail.com>
2025-08-07 17:25:35 +0200

    Update script2.py

--- annexes/code2/script2.py
+++ annexes/code2/script2.py
@@ -1,1 +1,10 @@
 
+# valence.py
+class ValenceEngine:
+    def compute_emotion(self, activation, valence):
+        return activation * valence
+
+# Test
+if __name__ == "__main__":
+    engine = ValenceEngine()
+    print(engine.compute_emotion(0.9, -0.5))  # -> -0.45
